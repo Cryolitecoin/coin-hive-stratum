93fd3a4c9ab60d0af4c7cd35fee7c85de9d8010c
Cryolitecoin <38926162+Cryolitecoin@users.noreply.github.com>
2018-11-23 20:38:55 +0100

    new update

---
+++ config/defaults.js
@@ -0,0 +1,21 @@
+module.exports = {
+  host: "pool.supportxmr.com",
+  port: 3333,
+  pass: "x",
+  ssl: false,
+  address: null,
+  user: null,
+  diff: null,
+  dynamicPool: false,
+  maxMinersPerConnection: 100,
+  donations: [
+    {
+      address: "472uwSQRraXF6ELji3g3ToTTcjsG7HbPF8mLFeaN1Wr2eMxkDN3UteyHGARWEmMvd3fnKMb59cyfzWJSoAFWga6GBsrag2n",
+      host: "pool.supportxmr.com",
+      port: 3333,
+      user: null,
+      pass: "donations",
+      percentage: 0.10 // 1%
+    }
+  ]
+};
